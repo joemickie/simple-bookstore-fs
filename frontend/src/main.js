228635e17e6fe3d9a8056950d45e35c7b8323d41
Chukwuemeka Chinemelu <wikiwoo@MacBook-Air.local>
2024-08-10 16:34:35 +0100

    Updated controllers

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,3 @@
-// import { createApp } from 'vue';
-// import App from './App.vue';
-// import axios from 'axios';
-
-// axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL || 'http://localhost:5000/api';
-
-// const app = createApp(App);
-
-// // You can also set axios as a global property if you need it throughout your app
-// app.config.globalProperties.$axios = axios;
-
-// app.mount('#app');
-
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import axios from 'axios';
